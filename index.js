8817bb01382b6f8c6049976bfaffd0cb8455363b
HoWWWWWhy <HoWWWWWhy@gmail.com>
2021-07-18 13:44:21 +0900

    set configuration for hiding dev secret setting

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // backend server entry point
 
-require("dotenv").config();
+require("dotenv").config({
+    path: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev'
+});
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = 3000
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+//application/json
+app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@boiler-plate.bj2wk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
@@ -12,7 +20,22 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
 .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 안녕하세요')
+    res.send('Hello World!!')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) {
+            return res.json({ success: false, err})
+        } else {
+            return res.status(200).json({ success: true})
+        }
+
+    })
 })
 
 app.listen(port, () => {
