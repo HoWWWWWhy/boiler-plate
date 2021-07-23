d47233ec4220135d63c0fa1d642466becbcf1cdc
HoWWWWWhy <HoWWWWWhy@gmail.com>
2021-07-23 23:48:01 +0900

    set Proxy Server

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function LandingPage() {
-    return (
-        <div>
-            Landing Page
-        </div>
-    )
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("/api/hello");
+      console.log(res);
+      setGreeting(res.data);
+    };
+    fetchData();
+    //axios.get("/api/hello").then((res) => console.log(res));
+  }, []);
+
+  return (
+    <>
+      <div>Landing Page</div>
+      <div>{greeting}</div>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
